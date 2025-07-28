78bcf93b1aa229da65069ff8dc3a2ce7b32a0a11
gaxxN5 <105100505+gaxxN5@users.noreply.github.com>
2025-07-28 18:19:00 +0900

    Delete pages directory

--- pages/3_About.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.markdown("""## About
-This WebApp is created by **クラッシャーバンバンFuji**.
-This WebApp is created for the purpose of learning Streamlit.
-I hope you enjoy this WebApp.
-""")
